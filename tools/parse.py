9dd17f890935a7a4c35d33f62898495ee3808a48
Josef Kuchař <josef.kuchar267@gmail.com>
2021-05-16 14:17:20 +0200

    Add parsing tools

---
+++ tools/parse.py
@@ -0,0 +1,32 @@
+import io
+import re
+import json
+
+lines = []
+
+with io.open('vyhlaska50.txt','r',encoding='utf8') as f:
+  for line in f.readlines():
+    line = line.strip();
+    if line == '':
+      continue
+    line = re.search(r"^(\d+).\t(.+):(.+)", line)
+
+    number = line[1].strip()
+    question = line[2].strip()
+    answer = line[3].strip()
+
+    if answer[0] == '-':
+      answer = answer[1:]
+    if answer[-1] == '.':
+      answer = answer[:-1]
+    answer = answer.strip()
+
+    data = {
+      'number': int(number),
+      'question': question,
+      'answer': answer
+    }
+
+    lines.append(data)
+
+print(json.dumps(lines))
